da35aa579adc75a33ffd90a73f74847e0895acc8
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-15 14:51:10 -0300

    setup socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import 'dotenv/config'
 import express from 'express'
+import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 import { join } from 'node:path'
 
 const port = process.env.PORT || 3000
 const app = express()
 const server = createServer(app)
+const io = new Server(server)
 
 app.use(express.static(join(__dirname, '..', 'public')))
 
 app.get('/', async (_, response) => {
   response.sendFile('index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log(socket.id + ' connected.')
 })
 
 server.listen(port, () => {
