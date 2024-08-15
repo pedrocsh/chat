93f35fac771ae7e3b58410c68b891171ebb21142
Pedro Cardoso <peedro.shell@gmail.com>
2024-08-15 14:46:56 -0300

    serve public files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import 'dotenv/config'
 import express from 'express'
+import { createServer } from 'node:http'
+import { join } from 'node:path'
 
 const port = process.env.PORT || 3000
 const app = express()
+const server = createServer(app)
 
-app.listen(port, () => {
+app.use(express.static(join(__dirname, '..', 'public')))
+
+app.get('/', async (_, response) => {
+  response.sendFile('index.html')
+})
+
+server.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
 
